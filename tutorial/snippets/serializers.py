97ca0c98d09c4b8f60db9e42bec9d6246d0298b2
June <42332051+6ixthmonth@users.noreply.github.com>
2023-05-25 10:20:21 +0900

    Update serializers.py

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -29,18 +29,18 @@
 #         return instance
 
 
-# from rest_framework import serializers
-# from snippets.models import Snippet
-
-
-# class SnippetSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Snippet
-#         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-
-
 from rest_framework import serializers
 from snippets.models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
